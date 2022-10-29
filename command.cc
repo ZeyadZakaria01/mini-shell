2d3722ab55def789bc1a823530b51310a1e8c33e
Zeyad <zeyad@gmail.com>
2022-10-29 17:29:23 +0200

    Finishing part2 pipes and redirecting

--- command.cc
+++ command.cc
@@ -22,217 +22,243 @@
 
 #include "command.h"
 
-SimpleCommand::SimpleCommand() {
-  // Create available space for 5 arguments
-  _numberOfAvailableArguments = 5;
-  _numberOfArguments = 0;
-  _arguments = (char **)malloc(_numberOfAvailableArguments * sizeof(char *));
+SimpleCommand::SimpleCommand()
+{
+	// Create available space for 5 arguments
+	_numberOfAvailableArguments = 5;
+	_numberOfArguments = 0;
+	_arguments = (char **)malloc(_numberOfAvailableArguments * sizeof(char *));
 }
 
-void SimpleCommand::insertArgument(char *argument) {
-  if (_numberOfAvailableArguments == _numberOfArguments + 1) {
-    // Double the available space
-    _numberOfAvailableArguments *= 2;
-    _arguments = (char **)realloc(_arguments,
-                                  _numberOfAvailableArguments * sizeof(char *));
-  }
+void SimpleCommand::insertArgument(char *argument)
+{
+	if (_numberOfAvailableArguments == _numberOfArguments + 1)
+	{
+		// Double the available space
+		_numberOfAvailableArguments *= 2;
+		_arguments = (char **)realloc(_arguments,
+									  _numberOfAvailableArguments * sizeof(char *));
+	}
 
-  _arguments[_numberOfArguments] = argument;
+	_arguments[_numberOfArguments] = argument;
 
-  // Add NULL argument at the end
-  _arguments[_numberOfArguments + 1] = NULL;
+	// Add NULL argument at the end
+	_arguments[_numberOfArguments + 1] = NULL;
 
-  _numberOfArguments++;
+	_numberOfArguments++;
 }
 
-Command::Command() {
-  // Create available space for one simple command
-  _numberOfAvailableSimpleCommands = 1;
-  _simpleCommands = (SimpleCommand **)malloc(_numberOfSimpleCommands *
-                                             sizeof(SimpleCommand *));
+Command::Command()
+{
+	// Create available space for one simple command
+	_numberOfAvailableSimpleCommands = 1;
+	_simpleCommands = (SimpleCommand **)malloc(_numberOfSimpleCommands *
+											   sizeof(SimpleCommand *));
 
-  _numberOfSimpleCommands = 0;
-  _outFile = 0;
-  _inputFile = 0;
-  _errFile = 0;
-  _background = 0;
-  _append = 0;
+	_numberOfSimpleCommands = 0;
+	_outFile = 0;
+	_inputFile = 0;
+	_errFile = 0;
+	_background = 0;
+	_append = 0;
 }
 
-void Command::insertSimpleCommand(SimpleCommand *simpleCommand) {
-  if (_numberOfAvailableSimpleCommands == _numberOfSimpleCommands) {
-    _numberOfAvailableSimpleCommands *= 2;
-    _simpleCommands = (SimpleCommand **)realloc(
-        _simpleCommands,
-        _numberOfAvailableSimpleCommands * sizeof(SimpleCommand *));
-  }
+void Command::insertSimpleCommand(SimpleCommand *simpleCommand)
+{
+	if (_numberOfAvailableSimpleCommands == _numberOfSimpleCommands)
+	{
+		_numberOfAvailableSimpleCommands *= 2;
+		_simpleCommands = (SimpleCommand **)realloc(
+			_simpleCommands,
+			_numberOfAvailableSimpleCommands * sizeof(SimpleCommand *));
+	}
 
-  _simpleCommands[_numberOfSimpleCommands] = simpleCommand;
-  _numberOfSimpleCommands++;
+	_simpleCommands[_numberOfSimpleCommands] = simpleCommand;
+	_numberOfSimpleCommands++;
 }
 
-void Command::clear() {
-  for (int i = 0; i < _numberOfSimpleCommands; i++) {
-    for (int j = 0; j < _simpleCommands[i]->_numberOfArguments; j++) {
-      free(_simpleCommands[i]->_arguments[j]);
-    }
+void Command::clear()
+{
+	for (int i = 0; i < _numberOfSimpleCommands; i++)
+	{
+		for (int j = 0; j < _simpleCommands[i]->_numberOfArguments; j++)
+		{
+			free(_simpleCommands[i]->_arguments[j]);
+		}
 
-    free(_simpleCommands[i]->_arguments);
-    free(_simpleCommands[i]);
-  }
+		free(_simpleCommands[i]->_arguments);
+		free(_simpleCommands[i]);
+	}
 
-  if (_outFile) {
-    free(_outFile);
-  }
+	if (_outFile)
+	{
+		free(_outFile);
+	}
 
-  if (_inputFile) {
-    free(_inputFile);
-  }
+	if (_inputFile)
+	{
+		free(_inputFile);
+	}
 
-  if (_errFile) {
-    free(_errFile);
-  }
+	if (_errFile)
+	{
+		free(_errFile);
+	}
 
-  _numberOfSimpleCommands = 0;
-  _outFile = 0;
-  _inputFile = 0;
-  _errFile = 0;
-  _background = 0;
+	_numberOfSimpleCommands = 0;
+	_outFile = 0;
+	_inputFile = 0;
+	_errFile = 0;
+	_background = 0;
 }
 
-void Command::print() {
-  printf("\n\n");
-  printf("              COMMAND TABLE                \n");
-  printf("\n");
-  printf("  #   Simple Commands\n");
-  printf("  --- ----------------------------------------------------------\n");
+void Command::print()
+{
+	printf("\n\n");
+	printf("              COMMAND TABLE                \n");
+	printf("\n");
+	printf("  #   Simple Commands\n");
+	printf("  --- ----------------------------------------------------------\n");
 
-  for (int i = 0; i < _numberOfSimpleCommands; i++) {
-    printf("  %-3d ", i);
-    for (int j = 0; j < _simpleCommands[i]->_numberOfArguments; j++) {
-      printf("\"%s\" \t", _simpleCommands[i]->_arguments[j]);
-    }
-  }
+	for (int i = 0; i < _numberOfSimpleCommands; i++)
+	{
+		printf("  %-3d ", i);
+		for (int j = 0; j < _simpleCommands[i]->_numberOfArguments; j++)
+		{
+			printf("\"%s\" \t", _simpleCommands[i]->_arguments[j]);
+		}
+	}
 
-  printf("\n\n");
-  printf("  Output       Input        Error        Background\n");
-  printf("  ------------ ------------ ------------ ------------\n");
-  printf("  %-12s %-12s %-12s %-12s\n", _outFile ? _outFile : "default",
-         _inputFile ? _inputFile : "default", _errFile ? _errFile : "default",
-         _background ? "YES" : "NO");
-  printf("\n\n");
+	printf("\n\n");
+	printf("  Output       Input        Error        Background\n");
+	printf("  ------------ ------------ ------------ ------------\n");
+	printf("  %-12s %-12s %-12s %-12s\n", _outFile ? _outFile : "default",
+		   _inputFile ? _inputFile : "default", _errFile ? _errFile : "default",
+		   _background ? "YES" : "NO");
+	printf("\n\n");
 }
 
-void Command::execute() {
-  // Don't do anything if there are no simple commands
-  if (_numberOfSimpleCommands == 0) {
-    prompt();
-    return;
-  }
+void Command::execute()
+{
+	// Don't do anything if there are no simple commands
+	if (_numberOfSimpleCommands == 0)
+	{
+		prompt();
+		return;
+	}
 
-  // Print contents of Command data structure
-  print();
+	// Print contents of Command data structure
+	print();
 
-  pid_t pid;
-  int status;
+	pid_t pid;
+	int status;
 
-  int defaultin = dup(STDIN_FILENO);   // Default file Descriptor for stdin
-  int defaultout = dup(STDOUT_FILENO); // Default file Descriptor for stdout
-  int defaulterr = dup(STDERR_FILENO); // Default file Descriptor for stderr
+	int defaultin = dup(STDIN_FILENO);	 // Default file Descriptor for stdin
+	int defaultout = dup(STDOUT_FILENO); // Default file Descriptor for stdout
+	int defaulterr = dup(STDERR_FILENO); // Default file Descriptor for stderr
 
-  /* int fdin, outfd; */
+	int fdin, outfd;
 
-  /* if (_inputFile) { */
-  /*   fdin = open(_inputFile, O_RDONLY); */
-  /*   if (fdin < 0) { */
-  /*     printf("Could not create file"); */
-  /*     exit(1); */
-  /*   } */
-  /*   // makes outdf file descriptor = 0 */
-  /*   fdin = dup2(fdin, STDIN_FILENO); */
-  /* } */
-  /* if (_outFile) { */
-  /*   outfd = creat(_outFile, 0600); */
-  /*   if (outfd < 0) { */
-  /*     printf("Could not create file"); */
-  /*     exit(1); */
-  /*   } */
-  /*   // makes outdf file descriptor = 1 */
-  /*   /1* dup2(outfd,2); *1/ */
-  /*   outfd = dup2(outfd, STDOUT_FILENO); */
-  /* } */
+	// handling _inputFile
+	if (_inputFile)
+	{
+		fdin = open(_inputFile, 0);
+		if (fdin < 0)
+		{
+			printf("Could not create file");
+			exit(1);
+		}
+	}
 
-  /* dup2(defaultin, 0); */
-  int fdpipes[_numberOfSimpleCommands][2];
+	// declaring our pipe
+	int fdpipes[2];
 
-  for (int i = 0; i < _numberOfSimpleCommands; i++) {
-    if (pipe(fdpipes[i]) == -1) {
-      printf("Error is piping\n");
-    }
-  }
+	for (int i = 0; i < _numberOfSimpleCommands; i++)
+	{
+		SimpleCommand *cmd = _simpleCommands[i];
 
+		pipe(fdpipes);
+		if (i != _numberOfSimpleCommands - 1)
+		{
+			dup2(fdpipes[1], 1);
+		}
+		else
+		{
+			if (_outFile)
+			{
+				outfd = creat(_outFile, 0600);
+				if (outfd < 0)
+				{
+					printf("Could not create file");
+					exit(1);
+				}
+				// makes outfd file descriptor = 1
+				dup2(outfd, 1);
+				close(outfd);
+			}
+			// default output if no _outFile
+			else
+			{
+				dup2(defaultout, 1);
+			}
+		}
+		close(fdpipes[1]);
+		pid = fork();
+		if (pid == -1)
+		{
+			printf("could not fork");
+			exit(EXIT_FAILURE);
+		}
+		// child
+		if (pid == 0)
+		{
+			close(fdpipes[0]);
+			close(defaultin);
+			close(defaultout);
+			close(defaulterr);
+			execvp(cmd->_arguments[0], cmd->_arguments);
+		}
+		dup2(fdpipes[0], 0);
+		close(fdpipes[0]);
+	}
 
-  for (int i = 0; i < _numberOfSimpleCommands; i++) {
-    // printf("i=%d",i);
-    SimpleCommand *cmd = _simpleCommands[i];
-   
-    pid = fork();
-    if (pid == -1) {
-      printf("could not fork");
-      exit(EXIT_FAILURE);
-    }
-    if (pid == 0) { // child
-     // if not first
-     // Input from previous output
-     printf(" my id is %d \n", getpid());
-      if (i != 0) {
-        if(dup2(fdpipes[i - 1][0], 0) < 0 )
-          exit(1);
-      }
-      // if not last command
-      // Output
-      if (i != _numberOfSimpleCommands - 1) {
-        if(dup2(fdpipes[i][1], 1) < 0 )
-          exit(1);
-      }     
-      execvp(cmd->_arguments[0], cmd->_arguments);
-    } 
-    else {
-      if (_background == 0) {
-        for (int j = 0; j < _numberOfSimpleCommands; j++) {
-        close(fdpipes[j][0]);
-        close(fdpipes[j][1]);
-        }
-        waitpid(pid, 0, 0);
-      }
-    }
-  }
-  for (int i = 0; i < _numberOfSimpleCommands; i++) {
-    close(fdpipes[i][0]);
-    close(fdpipes[i][1]);
-  }
-  // restore output to the previous state
-  dup2(defaultout, STDOUT_FILENO);
-  dup2(defaultin, STDIN_FILENO);
-  dup2(defaulterr, STDERR_FILENO);
+	// restoring to default input/output/error
+	dup2(defaultin, 0);
+	close(defaultin);
+	if (_inputFile != 0)
+	{
+		close(fdin);
+	}
 
-  /* close(fdin); */
-  /* close(outfd); */
-  /* dup2(fdin, STDOUT_FILENO); */
-  /* dup2(outfd, STDIN_FILENO); */
+	dup2(defaultout, 1);
+	close(defaultout);
+	if (_outFile)
+	{
+		close(outfd);
+	}
 
-  // Clear to prepare for next command
-  clear();
+	dup2(defaulterr, 2);
+	close(defaulterr);
 
-  // Print new prompt
-  prompt();
+	// handling parent wait
+	if (_background == 0)
+	{
+		waitpid(pid, &status, 0);
+	}
+
+	// Clear to prepare for next command
+	clear();
+
+	// Print new prompt
+	prompt();
 }
 
 // Shell implementation
 
-void Command::prompt() {
-  printf("myshell> ");
-  fflush(stdout);
+void Command::prompt()
+{
+	printf("myshell> ");
+	fflush(stdout);
 }
 
 Command Command::_currentCommand;
@@ -240,8 +266,9 @@ SimpleCommand *Command::_currentSimpleCommand;
 
 int yyparse(void);
 
-int main() {
-  Command::_currentCommand.prompt();
-  yyparse();
-  return 0;
+int main()
+{
+	Command::_currentCommand.prompt();
+	yyparse();
+	return 0;
 }
